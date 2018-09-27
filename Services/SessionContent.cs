5085164678d1dd1b3161f8264dabe91732b6c557
TheBigD6 <40880573+TheBigD6@users.noreply.github.com>
2018-09-27 15:34:57 +0200

    Add files via upload

---
+++ Services/SessionContent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolDemoWin10.Data;
+
+namespace SchoolDemoWin10.Services
+{
+    class SessionContent
+    {
+        public static int UserID;
+        public static string UserName;
+        public static Role UserRole;
+        public static Student CurrentStudent;
+        public static Teacher CurrentTeacher;
+    }
+}
